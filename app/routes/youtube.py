d9f138ab75c291b703dce963d2fee134a39e7c7b
chinpeerapat <88846925+chinpeerapat@users.noreply.github.com>
2025-03-15 13:25:01 +0700

    Add app/routes/youtube.py file

---
+++ app/routes/youtube.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+from app.models.youtube import YouTubeRequest
+from app.utils.youtube_tools import YouTubeTools
+
+router = APIRouter(
+    prefix="/youtube",
+    tags=["youtube"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.post("/video-data")
+async def get_video_data(request: YouTubeRequest):
+    """Endpoint to get video metadata"""
+    return YouTubeTools.get_video_data(request.url)
+
+@router.post("/video-captions")
+async def get_video_captions(request: YouTubeRequest):
+    """Endpoint to get video captions"""
+    return YouTubeTools.get_video_captions(request.url, request.languages)
+
+@router.post("/video-timestamps")
+async def get_video_timestamps(request: YouTubeRequest):
+    """Endpoint to get video timestamps"""
+    return YouTubeTools.get_video_timestamps(request.url, request.languages)
